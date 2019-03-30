018b6ae5503cdcf4d69982d38f7af0063b26e825
Brian Rogers <bobbymcr@gmail.com>
2019-03-30 08:15:49 -0700

    LetterBox - ReturnsNextVertices

--- src/Words.Core/LetterBox.cs
+++ src/Words.Core/LetterBox.cs
@@ -4,6 +4,8 @@
 
 namespace Words
 {
+    using System.Text;
+
     public sealed class LetterBox
     {
         private readonly string box;
@@ -14,5 +16,48 @@ namespace Words
         }
 
         public char this[int vertex] => this.box[vertex];
+
+        public Vertices Next(int start)
+        {
+            switch (start)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    return new Vertices(0b111111111000);
+                case 3:
+                case 4:
+                case 5:
+                    return new Vertices(0b111111000111);
+                case 6:
+                case 7:
+                case 8:
+                    return new Vertices(0b111000111111);
+                default:
+                    return new Vertices(0b000111111111);
+            }
+        }
+
+        public struct Vertices
+        {
+            private readonly ushort bits;
+
+            public Vertices(ushort bits)
+            {
+                this.bits = bits;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder(12);
+                for (int i = 0; i < 12; ++i)
+                {
+                    bool b = ((this.bits >> i) & 1) == 1;
+                    sb.Append(b ? '1' : '0');
+                }
+
+                return sb.ToString();
+            }
+        }
     }
 }
--- test/Words.Test/LetterBoxTest.cs
+++ test/Words.Test/LetterBoxTest.cs
@@ -14,7 +14,7 @@ namespace Words.Test
         [Fact]
         public void AllowsCharLookup()
         {
-            LetterBox box = new LetterBox("ABCDEFGHIJKL");
+            LetterBox box = New();
 
             Enumerable.Range(0, 12).Select(v => box[v]).Should().BeEquivalentTo(
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L');
@@ -25,12 +25,36 @@ namespace Words.Test
         [InlineData(12)]
         public void FailsCharLookupOutOfRange(int index)
         {
-            LetterBox box = new LetterBox("ABCDEFGHIJKL");
+            LetterBox box = New();
 
             char c;
             Action act = () => c = box[index];
 
             act.Should().Throw<IndexOutOfRangeException>();
         }
+
+        [Theory]
+        [InlineData(0, "000111111111")]
+        [InlineData(1, "000111111111")]
+        [InlineData(2, "000111111111")]
+        [InlineData(3, "111000111111")]
+        [InlineData(4, "111000111111")]
+        [InlineData(5, "111000111111")]
+        [InlineData(6, "111111000111")]
+        [InlineData(7, "111111000111")]
+        [InlineData(8, "111111000111")]
+        [InlineData(9, "111111111000")]
+        [InlineData(10, "111111111000")]
+        [InlineData(11, "111111111000")]
+        public void ReturnsNextVertices(int start, string expected)
+        {
+            LetterBox box = New();
+
+            LetterBox.Vertices verts = box.Next(start);
+
+            verts.ToString().Should().Be(expected);
+        }
+
+        private static LetterBox New() => new LetterBox("ABCDEFGHIJKL");
     }
 }
