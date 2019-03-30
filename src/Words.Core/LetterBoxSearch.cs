9f7a9cb0821852ea5c1560e375227935a7e95d58
Brian Rogers <bobbymcr@gmail.com>
2019-03-30 09:21:36 -0700

    LetterBoxSearch - OneValueTrieFindsOneWord

--- src/Words.Core/LetterBoxSearch.cs
+++ src/Words.Core/LetterBoxSearch.cs
@@ -19,6 +19,33 @@ namespace Words
 
         public void Run(Action<string, LetterBox.Vertices> found)
         {
+            int v = 0;
+            char c = this.box[v];
+            LetterBox.Vertices verts = new LetterBox.Vertices((ushort)(1 << v));
+            this.Next(this.trie[c], v, verts, found);
+        }
+
+        private void Next(StringTrie.INode node, int v1, LetterBox.Vertices verts, Action<string, LetterBox.Vertices> found)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.IsTerminal)
+            {
+                found(node.Value, verts);
+            }
+
+            LetterBox.Vertices next = this.box.Next(v1);
+            for (int v2 = 0; v2 < 12; ++v2)
+            {
+                if (next[v2])
+                {
+                    LetterBox.Vertices nextVerts = verts + new LetterBox.Vertices((ushort)(1 << v2));
+                    this.Next(node[this.box[v2]], v2, nextVerts, found);
+                }
+            }
         }
     }
 }
--- test/Words.Test/LetterBoxSearchTest.cs
+++ test/Words.Test/LetterBoxSearchTest.cs
@@ -4,6 +4,7 @@
 
 namespace Words.Test
 {
+    using System.Collections.Generic;
     using FluentAssertions;
     using Xunit;
 
@@ -19,6 +20,19 @@ namespace Words.Test
             search.Run((_, __) => ++count);
 
             count.Should().Be(0);
+        }
+
+        [Fact]
+        public void OneValueTrieFindsOneWord()
+        {
+            StringTrie trie = new StringTrie();
+            trie.Add("ALE");
+            LetterBoxSearch search = New(trie);
+            List<string> found = new List<string>();
+
+            search.Run((w, v) => found.Add(w + ":" + v));
+
+            found.Should().BeEquivalentTo("ALE:100010000001");
         }
 
         private static LetterBoxSearch New(StringTrie trie)
