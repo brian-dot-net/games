0b083d2ebcb031d2715e8908d86c5d5e4e8091de
Brian Rogers <bobbymcr@gmail.com>
2019-04-02 18:56:25 -0700

    StrTrie - Empty

---
+++ src/Words.Core/StrTrie.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="StrTrie.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Words
+{
+    public sealed class StrTrie
+    {
+        public int Count => 0;
+    }
+}
---
+++ test/Words.Test/StrTrieTest.cs
@@ -0,0 +1,20 @@
+// <copyright file="StrTrieTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Words.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public sealed class StrTrieTest
+    {
+        [Fact]
+        public void Empty()
+        {
+            StrTrie trie = new StrTrie();
+
+            trie.Count.Should().Be(0);
+        }
+    }
+}
