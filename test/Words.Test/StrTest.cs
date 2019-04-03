ff11f4f28fa65c28c68de3cf3bdeb65411416f20
Brian Rogers <bobbymcr@gmail.com>
2019-04-02 18:42:59 -0700

    Str - EightChars

--- test/Words.Test/StrTest.cs
+++ test/Words.Test/StrTest.cs
@@ -113,5 +113,38 @@ namespace Words.Test
                 Ch.None,
                 Ch.None);
         }
+
+        [Theory]
+        [InlineData(Ch.V, Ch.W, Ch.X, Ch.Y, Ch.Z, Ch.A, Ch.B, Ch.C, "VWXYZABC")]
+        [InlineData(Ch.A, Ch.B, Ch.C, Ch.D, Ch.E, Ch.F, Ch.G, Ch.H, "ABCDEFGH")]
+        [InlineData(Ch.I, Ch.J, Ch.K, Ch.L, Ch.M, Ch.N, Ch.O, Ch.P, "IJKLMNOP")]
+        public void EightChars(Ch c0, Ch c1, Ch c2, Ch c3, Ch c4, Ch c5, Ch c6, Ch c7, string expected)
+        {
+            Str s = default(Str)
+                .Append(c0)
+                .Append(c1)
+                .Append(c2)
+                .Append(c3)
+                .Append(c4)
+                .Append(c5)
+                .Append(c6)
+                .Append(c7);
+
+            s.Length.Should().Be(8);
+            s.ToString().Should().Be(expected);
+            Enumerable.Range(0, 12).Select(i => s[i]).Should().BeEquivalentTo(
+                c0,
+                c1,
+                c2,
+                c3,
+                c4,
+                c5,
+                c6,
+                c7,
+                Ch.None,
+                Ch.None,
+                Ch.None,
+                Ch.None);
+        }
     }
 }
