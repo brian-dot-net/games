9db2c8dda89a6e3ca384bfe53b668bfe511c0aef
Brian Rogers <bobbymcr@gmail.com>
2019-04-02 19:36:39 -0700

    StrTrie - TwoItemsLength2SharedPrefix

--- src/Words.Core/StrTrie.cs
+++ src/Words.Core/StrTrie.cs
@@ -4,13 +4,47 @@
 
 namespace Words
 {
+    using System;
+    using System.Collections.Generic;
+
     public sealed class StrTrie
     {
+        private readonly Dictionary<Str, bool> nodes;
+
+        public StrTrie()
+        {
+            this.nodes = new Dictionary<Str, bool>();
+        }
+
+        public enum NodeKind : byte
+        {
+            None = 0,
+            Prefix = 1,
+            Terminal = 2,
+        }
+
         public int Count { get; private set; }
 
         public void Add(Str value)
         {
             ++this.Count;
+            this.nodes[value] = true;
+            for (byte i = 1; i < value.Length; ++i)
+            {
+                value = value.Chop();
+                this.nodes[value] = false;
+            }
+        }
+
+        public NodeKind Find(Str value)
+        {
+            bool isTerminal;
+            if (!this.nodes.TryGetValue(value, out isTerminal))
+            {
+                return NodeKind.None;
+            }
+
+            return isTerminal ? NodeKind.Terminal : NodeKind.Prefix;
         }
     }
 }
--- test/Words.Test/StrTrieTest.cs
+++ test/Words.Test/StrTrieTest.cs
@@ -26,5 +26,19 @@ namespace Words.Test
 
             trie.Count.Should().Be(1);
         }
+
+        [Fact]
+        public void TwoItemsLength2SharedPrefix()
+        {
+            StrTrie trie = new StrTrie();
+
+            trie.Add(default(Str).Append(Ch.H).Append(Ch.I));
+            trie.Add(default(Str).Append(Ch.H).Append(Ch.A));
+
+            trie.Count.Should().Be(2);
+            trie.Find(default(Str).Append(Ch.H)).Should().Be(StrTrie.NodeKind.Prefix);
+            trie.Find(default(Str).Append(Ch.H).Append(Ch.A)).Should().Be(StrTrie.NodeKind.Terminal);
+            trie.Find(default(Str).Append(Ch.H).Append(Ch.I)).Should().Be(StrTrie.NodeKind.Terminal);
+        }
     }
 }
