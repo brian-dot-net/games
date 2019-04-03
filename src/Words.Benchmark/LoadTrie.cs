353d0886ca33819601854cc43d42dfc228602463
Brian Rogers <bobbymcr@gmail.com>
2019-04-02 17:45:29 -0700

    Add Words.Benchmark project with LoadTrie benchmark

---
+++ src/Words.Benchmark/LoadTrie.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="LoadTrie.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Words.Benchmark
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using BenchmarkDotNet.Attributes;
+
+    [CoreJob]
+    [MemoryDiagnoser]
+    public class LoadTrie
+    {
+        [Params(25, 50, 100)]
+        public int Pct { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            string[] lines = File.ReadAllLines("words.txt");
+
+            List<string> lines25 = new List<string>();
+            List<string> lines50 = new List<string>();
+            List<string> lines100 = new List<string>();
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                string line = lines[i];
+                int n = i % 16;
+                if (n < 4)
+                {
+                    lines25.Add(line);
+                }
+
+                if (n < 8)
+                {
+                    lines50.Add(line);
+                }
+
+                if (n < 16)
+                {
+                    lines100.Add(line);
+                }
+            }
+
+            File.WriteAllLines(FileName(25), lines25);
+            File.WriteAllLines(FileName(50), lines50);
+            File.WriteAllLines(FileName(100), lines100);
+        }
+
+        [Benchmark]
+        public int FromFile()
+        {
+            StringTrie trie = StringTrie.Load(File.OpenRead(FileName(this.Pct)));
+            return trie.Count;
+        }
+
+        private static string FileName(int pct) => $"words_{pct}.txt";
+    }
+}
---
+++ src/Words.Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Words.Benchmark
+{
+    using BenchmarkDotNet.Running;
+
+    internal sealed class Program
+    {
+        private static void Main()
+        {
+            BenchmarkRunner.Run<LoadTrie>();
+        }
+    }
+}
