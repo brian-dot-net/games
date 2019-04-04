9d260ce1c2d6adebbf33719d1c02a2059d6cba3a
Brian Rogers <bobbymcr@gmail.com>
2019-04-03 21:51:47 -0700

    LetterBoxStr - FailsCharLookupOutOfRange

--- test/Words.Test/LetterBoxStrTest.cs
+++ test/Words.Test/LetterBoxStrTest.cs
@@ -4,6 +4,7 @@
 
 namespace Words.Test
 {
+    using System;
     using System.Linq;
     using FluentAssertions;
     using Xunit;
@@ -17,6 +18,19 @@ namespace Words.Test
 
             Enumerable.Range(0, 12).Select(v => box[(byte)v]).Should().BeEquivalentTo(
                 Ch.A, Ch.B, Ch.C, Ch.D, Ch.E, Ch.F, Ch.G, Ch.H, Ch.I, Ch.J, Ch.K, Ch.L);
+        }
+
+        [Theory]
+        [InlineData(12)]
+        [InlineData(255)]
+        public void FailsCharLookupOutOfRange(byte index)
+        {
+            LetterBoxStr box = New();
+
+            Ch c;
+            Action act = () => c = box[index];
+
+            act.Should().Throw<IndexOutOfRangeException>();
         }
 
         private static LetterBoxStr New()
