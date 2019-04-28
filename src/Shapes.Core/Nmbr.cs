585b49b0b26078a5c9be0f35c46d873219ab8aa9
Brian Rogers <bobbymcr@gmail.com>
2019-04-28 00:01:21 -0700

    Nmbr - Value

--- src/Shapes.Core/Nmbr.cs
+++ src/Shapes.Core/Nmbr.cs
@@ -11,61 +11,65 @@ namespace Shapes
     {
         public const byte Side = 4;
 
-        public static readonly Nmbr Zero0 = new Nmbr(0x7557);
-        public static readonly Nmbr Zero1 = new Nmbr(0x0F9F);
-        public static readonly Nmbr One0 = new Nmbr(0x2223);
-        public static readonly Nmbr One1 = new Nmbr(0x00F8);
-        public static readonly Nmbr One2 = new Nmbr(0x3111);
-        public static readonly Nmbr One3 = new Nmbr(0x001F);
-        public static readonly Nmbr Two0 = new Nmbr(0x7366);
-        public static readonly Nmbr Two1 = new Nmbr(0x0DF7);
-        public static readonly Nmbr Two2 = new Nmbr(0x3767);
-        public static readonly Nmbr Two3 = new Nmbr(0x0CFB);
-        public static readonly Nmbr Three0 = new Nmbr(0x7647);
-        public static readonly Nmbr Three1 = new Nmbr(0x0FB9);
-        public static readonly Nmbr Three2 = new Nmbr(0x7137);
-        public static readonly Nmbr Three3 = new Nmbr(0x09DF);
-        public static readonly Nmbr Four0 = new Nmbr(0x6726);
-        public static readonly Nmbr Four1 = new Nmbr(0x0BF2);
-        public static readonly Nmbr Four2 = new Nmbr(0x3273);
-        public static readonly Nmbr Four3 = new Nmbr(0x04FD);
-        public static readonly Nmbr Five0 = new Nmbr(0x7477);
-        public static readonly Nmbr Five1 = new Nmbr(0x0FDD);
-        public static readonly Nmbr Five2 = new Nmbr(0x7717);
-        public static readonly Nmbr Five3 = new Nmbr(0x0BBF);
-        public static readonly Nmbr Six0 = new Nmbr(0x7713);
-        public static readonly Nmbr Six1 = new Nmbr(0x03BF);
-        public static readonly Nmbr Six2 = new Nmbr(0x6477);
-        public static readonly Nmbr Six3 = new Nmbr(0x0FDC);
-        public static readonly Nmbr Seven0 = new Nmbr(0x1327);
-        public static readonly Nmbr Seven1 = new Nmbr(0x08EB);
-        public static readonly Nmbr Seven2 = new Nmbr(0x7264);
-        public static readonly Nmbr Seven3 = new Nmbr(0x0D71);
-        public static readonly Nmbr Eight0 = new Nmbr(0x3366);
-        public static readonly Nmbr Eight1 = new Nmbr(0x0CF3);
-        public static readonly Nmbr Nine0 = new Nmbr(0x3377);
-        public static readonly Nmbr Nine1 = new Nmbr(0x0CFF);
-        public static readonly Nmbr Nine2 = new Nmbr(0x7766);
-        public static readonly Nmbr Nine3 = new Nmbr(0x0FF3);
+        public static readonly Nmbr Zero0 = new Nmbr(0x7557, 0);
+        public static readonly Nmbr Zero1 = new Nmbr(0x0F9F, 0);
+        public static readonly Nmbr One0 = new Nmbr(0x2223, 1);
+        public static readonly Nmbr One1 = new Nmbr(0x00F8, 1);
+        public static readonly Nmbr One2 = new Nmbr(0x3111, 1);
+        public static readonly Nmbr One3 = new Nmbr(0x001F, 1);
+        public static readonly Nmbr Two0 = new Nmbr(0x7366, 2);
+        public static readonly Nmbr Two1 = new Nmbr(0x0DF7, 2);
+        public static readonly Nmbr Two2 = new Nmbr(0x3767, 2);
+        public static readonly Nmbr Two3 = new Nmbr(0x0CFB, 2);
+        public static readonly Nmbr Three0 = new Nmbr(0x7647, 3);
+        public static readonly Nmbr Three1 = new Nmbr(0x0FB9, 3);
+        public static readonly Nmbr Three2 = new Nmbr(0x7137, 3);
+        public static readonly Nmbr Three3 = new Nmbr(0x09DF, 3);
+        public static readonly Nmbr Four0 = new Nmbr(0x6726, 4);
+        public static readonly Nmbr Four1 = new Nmbr(0x0BF2, 4);
+        public static readonly Nmbr Four2 = new Nmbr(0x3273, 4);
+        public static readonly Nmbr Four3 = new Nmbr(0x04FD, 4);
+        public static readonly Nmbr Five0 = new Nmbr(0x7477, 5);
+        public static readonly Nmbr Five1 = new Nmbr(0x0FDD, 5);
+        public static readonly Nmbr Five2 = new Nmbr(0x7717, 5);
+        public static readonly Nmbr Five3 = new Nmbr(0x0BBF, 5);
+        public static readonly Nmbr Six0 = new Nmbr(0x7713, 6);
+        public static readonly Nmbr Six1 = new Nmbr(0x03BF, 6);
+        public static readonly Nmbr Six2 = new Nmbr(0x6477, 6);
+        public static readonly Nmbr Six3 = new Nmbr(0x0FDC, 6);
+        public static readonly Nmbr Seven0 = new Nmbr(0x1327, 7);
+        public static readonly Nmbr Seven1 = new Nmbr(0x08EB, 7);
+        public static readonly Nmbr Seven2 = new Nmbr(0x7264, 7);
+        public static readonly Nmbr Seven3 = new Nmbr(0x0D71, 7);
+        public static readonly Nmbr Eight0 = new Nmbr(0x3366, 8);
+        public static readonly Nmbr Eight1 = new Nmbr(0x0CF3, 8);
+        public static readonly Nmbr Nine0 = new Nmbr(0x3377, 9);
+        public static readonly Nmbr Nine1 = new Nmbr(0x0CFF, 9);
+        public static readonly Nmbr Nine2 = new Nmbr(0x7766, 9);
+        public static readonly Nmbr Nine3 = new Nmbr(0x0FF3, 9);
 
-        private readonly ushort value;
+        private readonly ushort bits;
 
-        private Nmbr(ushort value)
+        private Nmbr(ushort bits, byte value)
         {
-            this.value = value;
+            this.bits = bits;
+            this.Value = value;
         }
 
-        public bool this[int x, int y] => ((this.value >> ((y * Side) + x)) & 1) == 1;
+        public byte Value { get; }
+
+        public bool this[int x, int y] => ((this.bits >> ((y * Side) + x)) & 1) == 1;
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder(40);
-            ushort v = this.value;
+            ushort v = this.bits;
+            char d = (char)(this.Value + '0');
             for (int j = 0; j < 4; ++j)
             {
                 for (int i = 0; i < 4; ++i)
                 {
-                    char c = (v & 1) == 1 ? 'x' : '.';
+                    char c = (v & 1) == 1 ? d : '.';
                     sb.Append(c);
                     sb.Append(' ');
                     v >>= 1;
--- test/Shapes.Test/NmbrTest.cs
+++ test/Shapes.Test/NmbrTest.cs
@@ -13,10 +13,10 @@ namespace Shapes.Test
         public void Zero0()
         {
             const string Expected = @"
-x x x . 
-x . x . 
-x . x . 
-x x x . 
+0 0 0 . 
+0 . 0 . 
+0 . 0 . 
+0 0 0 . 
 ";
             ShouldBeString(Nmbr.Zero0, Expected);
         }
@@ -25,9 +25,9 @@ x x x .
         public void Zero1()
         {
             const string Expected = @"
-x x x x 
-x . . x 
-x x x x 
+0 0 0 0 
+0 . . 0 
+0 0 0 0 
 . . . . 
 ";
             ShouldBeString(Nmbr.Zero1, Expected);
@@ -36,409 +36,409 @@ x x x x
         [Fact]
         public void One0()
         {
-            const string Expected = @"
-x x . . 
-. x . . 
-. x . . 
-. x . . 
+            const string E1pected = @"
+1 1 . . 
+. 1 . . 
+. 1 . . 
+. 1 . . 
 ";
-            ShouldBeString(Nmbr.One0, Expected);
+            ShouldBeString(Nmbr.One0, E1pected);
         }
 
         [Fact]
         public void One1()
         {
-            const string Expected = @"
-. . . x 
-x x x x 
+            const string E1pected = @"
+. . . 1 
+1 1 1 1 
 . . . . 
 . . . . 
 ";
-            ShouldBeString(Nmbr.One1, Expected);
+            ShouldBeString(Nmbr.One1, E1pected);
         }
 
         [Fact]
         public void One2()
         {
-            const string Expected = @"
-x . . . 
-x . . . 
-x . . . 
-x x . . 
+            const string E1pected = @"
+1 . . . 
+1 . . . 
+1 . . . 
+1 1 . . 
 ";
-            ShouldBeString(Nmbr.One2, Expected);
+            ShouldBeString(Nmbr.One2, E1pected);
         }
 
         [Fact]
         public void One3()
         {
-            const string Expected = @"
-x x x x 
-x . . . 
+            const string E1pected = @"
+1 1 1 1 
+1 . . . 
 . . . . 
 . . . . 
 ";
-            ShouldBeString(Nmbr.One3, Expected);
+            ShouldBeString(Nmbr.One3, E1pected);
         }
 
         [Fact]
         public void Two0()
         {
-            const string Expected = @"
-. x x . 
-. x x . 
-x x . . 
-x x x . 
+            const string E2pected = @"
+. 2 2 . 
+. 2 2 . 
+2 2 . . 
+2 2 2 . 
 ";
-            ShouldBeString(Nmbr.Two0, Expected);
+            ShouldBeString(Nmbr.Two0, E2pected);
         }
 
         [Fact]
         public void Two1()
         {
-            const string Expected = @"
-x x x . 
-x x x x 
-x . x x 
+            const string E2pected = @"
+2 2 2 . 
+2 2 2 2 
+2 . 2 2 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Two1, Expected);
+            ShouldBeString(Nmbr.Two1, E2pected);
         }
 
         [Fact]
         public void Two2()
         {
-            const string Expected = @"
-x x x . 
-. x x . 
-x x x . 
-x x . . 
+            const string E2pected = @"
+2 2 2 . 
+. 2 2 . 
+2 2 2 . 
+2 2 . . 
 ";
-            ShouldBeString(Nmbr.Two2, Expected);
+            ShouldBeString(Nmbr.Two2, E2pected);
         }
 
         [Fact]
         public void Two3()
         {
-            const string Expected = @"
-x x . x 
-x x x x 
-. . x x 
+            const string E2pected = @"
+2 2 . 2 
+2 2 2 2 
+. . 2 2 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Two3, Expected);
+            ShouldBeString(Nmbr.Two3, E2pected);
         }
 
         [Fact]
         public void Three0()
         {
-            const string Expected = @"
-x x x . 
-. . x . 
-. x x . 
-x x x . 
+            const string E3pected = @"
+3 3 3 . 
+. . 3 . 
+. 3 3 . 
+3 3 3 . 
 ";
-            ShouldBeString(Nmbr.Three0, Expected);
+            ShouldBeString(Nmbr.Three0, E3pected);
         }
 
         [Fact]
         public void Three1()
         {
-            const string Expected = @"
-x . . x 
-x x . x 
-x x x x 
+            const string E3pected = @"
+3 . . 3 
+3 3 . 3 
+3 3 3 3 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Three1, Expected);
+            ShouldBeString(Nmbr.Three1, E3pected);
         }
 
         [Fact]
         public void Three2()
         {
-            const string Expected = @"
-x x x . 
-x x . . 
-x . . . 
-x x x . 
+            const string E3pected = @"
+3 3 3 . 
+3 3 . . 
+3 . . . 
+3 3 3 . 
 ";
-            ShouldBeString(Nmbr.Three2, Expected);
+            ShouldBeString(Nmbr.Three2, E3pected);
         }
 
         [Fact]
         public void Three3()
         {
-            const string Expected = @"
-x x x x 
-x . x x 
-x . . x 
+            const string E3pected = @"
+3 3 3 3 
+3 . 3 3 
+3 . . 3 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Three3, Expected);
+            ShouldBeString(Nmbr.Three3, E3pected);
         }
 
         [Fact]
         public void Four0()
         {
-            const string Expected = @"
-. x x . 
-. x . . 
-x x x . 
-. x x . 
+            const string E4pected = @"
+. 4 4 . 
+. 4 . . 
+4 4 4 . 
+. 4 4 . 
 ";
-            ShouldBeString(Nmbr.Four0, Expected);
+            ShouldBeString(Nmbr.Four0, E4pected);
         }
 
         [Fact]
         public void Four1()
         {
-            const string Expected = @"
-. x . . 
-x x x x 
-x x . x 
+            const string E4pected = @"
+. 4 . . 
+4 4 4 4 
+4 4 . 4 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Four1, Expected);
+            ShouldBeString(Nmbr.Four1, E4pected);
         }
 
         [Fact]
         public void Four2()
         {
-            const string Expected = @"
-x x . . 
-x x x . 
-. x . . 
-x x . . 
+            const string E4pected = @"
+4 4 . . 
+4 4 4 . 
+. 4 . . 
+4 4 . . 
 ";
-            ShouldBeString(Nmbr.Four2, Expected);
+            ShouldBeString(Nmbr.Four2, E4pected);
         }
 
         [Fact]
         public void Four3()
         {
-            const string Expected = @"
-x . x x 
-x x x x 
-. . x . 
+            const string E4pected = @"
+4 . 4 4 
+4 4 4 4 
+. . 4 . 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Four3, Expected);
+            ShouldBeString(Nmbr.Four3, E4pected);
         }
 
         [Fact]
         public void Five0()
         {
-            const string Expected = @"
-x x x . 
-x x x . 
-. . x . 
-x x x . 
+            const string E5pected = @"
+5 5 5 . 
+5 5 5 . 
+. . 5 . 
+5 5 5 . 
 ";
-            ShouldBeString(Nmbr.Five0, Expected);
+            ShouldBeString(Nmbr.Five0, E5pected);
         }
 
         [Fact]
         public void Five1()
         {
-            const string Expected = @"
-x . x x 
-x . x x 
-x x x x 
+            const string E5pected = @"
+5 . 5 5 
+5 . 5 5 
+5 5 5 5 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Five1, Expected);
+            ShouldBeString(Nmbr.Five1, E5pected);
         }
 
         [Fact]
         public void Five2()
         {
-            const string Expected = @"
-x x x . 
-x . . . 
-x x x . 
-x x x . 
+            const string E5pected = @"
+5 5 5 . 
+5 . . . 
+5 5 5 . 
+5 5 5 . 
 ";
-            ShouldBeString(Nmbr.Five2, Expected);
+            ShouldBeString(Nmbr.Five2, E5pected);
         }
 
         [Fact]
         public void Five3()
         {
-            const string Expected = @"
-x x x x 
-x x . x 
-x x . x 
+            const string E5pected = @"
+5 5 5 5 
+5 5 . 5 
+5 5 . 5 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Five3, Expected);
+            ShouldBeString(Nmbr.Five3, E5pected);
         }
 
         [Fact]
         public void Six0()
         {
-            const string Expected = @"
-x x . . 
-x . . . 
-x x x . 
-x x x . 
+            const string E6pected = @"
+6 6 . . 
+6 . . . 
+6 6 6 . 
+6 6 6 . 
 ";
-            ShouldBeString(Nmbr.Six0, Expected);
+            ShouldBeString(Nmbr.Six0, E6pected);
         }
 
         [Fact]
         public void Six1()
         {
-            const string Expected = @"
-x x x x 
-x x . x 
-x x . . 
+            const string E6pected = @"
+6 6 6 6 
+6 6 . 6 
+6 6 . . 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Six1, Expected);
+            ShouldBeString(Nmbr.Six1, E6pected);
         }
 
         [Fact]
         public void Six2()
         {
-            const string Expected = @"
-x x x . 
-x x x . 
-. . x . 
-. x x . 
+            const string E6pected = @"
+6 6 6 . 
+6 6 6 . 
+. . 6 . 
+. 6 6 . 
 ";
-            ShouldBeString(Nmbr.Six2, Expected);
+            ShouldBeString(Nmbr.Six2, E6pected);
         }
 
         [Fact]
         public void Six3()
         {
-            const string Expected = @"
-. . x x 
-x . x x 
-x x x x 
+            const string E6pected = @"
+. . 6 6 
+6 . 6 6 
+6 6 6 6 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Six3, Expected);
+            ShouldBeString(Nmbr.Six3, E6pected);
         }
 
         [Fact]
         public void Seven0()
         {
-            const string Expected = @"
-x x x . 
-. x . . 
-x x . . 
-x . . . 
+            const string E7pected = @"
+7 7 7 . 
+. 7 . . 
+7 7 . . 
+7 . . . 
 ";
-            ShouldBeString(Nmbr.Seven0, Expected);
+            ShouldBeString(Nmbr.Seven0, E7pected);
         }
 
         [Fact]
         public void Seven1()
         {
-            const string Expected = @"
-x x . x 
-. x x x 
-. . . x 
+            const string E7pected = @"
+7 7 . 7 
+. 7 7 7 
+. . . 7 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Seven1, Expected);
+            ShouldBeString(Nmbr.Seven1, E7pected);
         }
 
         [Fact]
         public void Seven2()
         {
-            const string Expected = @"
-. . x . 
-. x x . 
-. x . . 
-x x x . 
+            const string E7pected = @"
+. . 7 . 
+. 7 7 . 
+. 7 . . 
+7 7 7 . 
 ";
-            ShouldBeString(Nmbr.Seven2, Expected);
+            ShouldBeString(Nmbr.Seven2, E7pected);
         }
 
         [Fact]
         public void Seven3()
         {
-            const string Expected = @"
-x . . . 
-x x x . 
-x . x x 
+            const string E7pected = @"
+7 . . . 
+7 7 7 . 
+7 . 7 7 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Seven3, Expected);
+            ShouldBeString(Nmbr.Seven3, E7pected);
         }
 
         [Fact]
         public void Eight0()
         {
-            const string Expected = @"
-. x x . 
-. x x . 
-x x . . 
-x x . . 
+            const string E8pected = @"
+. 8 8 . 
+. 8 8 . 
+8 8 . . 
+8 8 . . 
 ";
-            ShouldBeString(Nmbr.Eight0, Expected);
+            ShouldBeString(Nmbr.Eight0, E8pected);
         }
 
         [Fact]
         public void Eight1()
         {
-            const string Expected = @"
-x x . . 
-x x x x 
-. . x x 
+            const string E8pected = @"
+8 8 . . 
+8 8 8 8 
+. . 8 8 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Eight1, Expected);
+            ShouldBeString(Nmbr.Eight1, E8pected);
         }
 
         [Fact]
         public void Nine0()
         {
-            const string Expected = @"
-x x x . 
-x x x . 
-x x . . 
-x x . . 
+            const string E9pected = @"
+9 9 9 . 
+9 9 9 . 
+9 9 . . 
+9 9 . . 
 ";
-            ShouldBeString(Nmbr.Nine0, Expected);
+            ShouldBeString(Nmbr.Nine0, E9pected);
         }
 
         [Fact]
         public void Nine1()
         {
-            const string Expected = @"
-x x x x 
-x x x x 
-. . x x 
+            const string E9pected = @"
+9 9 9 9 
+9 9 9 9 
+. . 9 9 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Nine1, Expected);
+            ShouldBeString(Nmbr.Nine1, E9pected);
         }
 
         [Fact]
         public void Nine2()
         {
-            const string Expected = @"
-. x x . 
-. x x . 
-x x x . 
-x x x . 
+            const string E9pected = @"
+. 9 9 . 
+. 9 9 . 
+9 9 9 . 
+9 9 9 . 
 ";
-            ShouldBeString(Nmbr.Nine2, Expected);
+            ShouldBeString(Nmbr.Nine2, E9pected);
         }
 
         [Fact]
         public void Nine3()
         {
-            const string Expected = @"
-x x . . 
-x x x x 
-x x x x 
+            const string E9pected = @"
+9 9 . . 
+9 9 9 9 
+9 9 9 9 
 . . . . 
 ";
-            ShouldBeString(Nmbr.Nine3, Expected);
+            ShouldBeString(Nmbr.Nine3, E9pected);
         }
 
         private static void ShouldBeString(Nmbr n, string expected)
