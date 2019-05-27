09c73567a44c3264324933f625e2862d90991fe7
Brian Rogers <bobbymcr@gmail.com>
2019-05-27 12:17:22 -0700

    Complete native solver implementation

--- src/LetterBoxedSolver.Native/LetterBoxedSolver.cpp
+++ src/LetterBoxedSolver.Native/LetterBoxedSolver.cpp
@@ -1,5 +1,5 @@
-#include "StrTrie.h"
-#include "LetterBoxStr.h"
+#include "LetterBoxStrSearch.h"
+#include "LetterBoxStrWords.h"
 #include "Stopwatch.h"
 #include <iomanip>
 #include <fstream>
@@ -39,6 +39,21 @@ int main(int argc, const char** argv)
     stringstream ss;
     ss << "Loaded " << trie.size() << " words.";
     Log(ss.str().c_str());
+
+    LetterBoxStrSearch search(trie, box);
+    LetterBoxStrWords words;
+
+    Log("Finding valid words...");
+    search.run([&words](Str w, Vertices v) { words.insert(w, v); });
+    ss = stringstream();
+    ss << "Found " << words.size() << " valid words.";
+    Log(ss.str().c_str());
+    ss.clear();
+
+    Log("Finding solutions...");
+    words.find([](Str w1, Str w2) { cout << w1 << "-" << w2 << "\r\n"; });
+
+    Log("Done.");
 
     return 0;
 }
