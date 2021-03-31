3d1328a08c4ac1436e548d328ca42ae7f2d77a19
Brian Rogers <bobbymcr@gmail.com>
2021-03-31 12:31:08 -0700

    letter_boxed_solver: StTrie one_item_len_1()

--- letter_boxed_solver/src/trie.rs
+++ letter_boxed_solver/src/trie.rs
@@ -1,12 +1,18 @@
-pub struct StTrie;
+use crate::core::St;
+
+pub struct StTrie(usize);
 
 impl StTrie {
     fn new() -> StTrie {
-        StTrie
+        StTrie(0)
     }
 
     fn len(&self) -> usize {
-        0
+        self.0
+    }
+
+    fn insert(&mut self, _item: St) {
+        self.0 += 1;
     }
 }
 
@@ -19,5 +25,14 @@ mod tests {
         let trie = StTrie::new();
 
         assert_eq!(0, trie.len());
+    }
+
+    #[test]
+    fn one_item_len_1() {
+        let mut trie = StTrie::new();
+
+        trie.insert("X".parse::<St>().unwrap());
+
+        assert_eq!(1, trie.len());
     }
 }
